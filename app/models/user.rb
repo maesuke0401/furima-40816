f8e3fad42013bd9ec8bc6d16225f020d743f7278
maesuke0401 <166385799+maesuke0401@users.noreply.github.com>
2024-05-17 16:23:48 +0900

    新規登録とログイン、ログアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :items
+  has_many :records
+
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         attribute :birthday, :date
+         attribute :last_name, :string
+         attribute :first_name, :string
+         attribute :next_last_name, :string
+         attribute :next_first_name, :string
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_15_090548) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_15_104351) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "product", null: false
+    t.integer "categry_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "delivery_fee_id", null: false
+    t.integer "source_id", null: false
+    t.integer "price_id", null: false
+    t.string "shipping_id", null: false
+    t.text "description", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_15_090548) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
