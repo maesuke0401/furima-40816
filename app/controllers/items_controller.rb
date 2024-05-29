8c8d9d4a539384c20950920bc5c9997b71fcea56
maesuke0401 <166385799+maesuke0401@users.noreply.github.com>
2024-05-29 16:22:54 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :kosuke, only: [:show, :edit, :update]
   def index
     @items = Item.all.order(created_at: :desc)
  end
@@ -8,7 +9,7 @@ class ItemsController < ApplicationController
 
 end
 def show
-  @item = Item.find(params[:id])
+ 
 end
 
 def create
@@ -21,14 +22,14 @@ def create
 end
 
 def edit
-  @item = Item.find(params[:id])
+
   if user_signed_in? && current_user.id != @item.user_id
     redirect_to root_path
   end
 end
 
 def update
-  @item = Item.find(params[:id])
+
   if @item.update(items_params)
   redirect_to item_path
   else
@@ -40,6 +41,10 @@ private
 
   def items_params
     params.require(:item).permit(:product, :description, :category_id, :condition_id, :delivery_fee_id, :source_id, :shipping_id, :price_id, :image).merge(user_id: current_user.id)
+  end
+
+  def kosuke
+    @item = Item.find(params[:id])
   end
 
 end
