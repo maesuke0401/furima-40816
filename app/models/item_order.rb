65ac28df46a4bdab98312a1a9c7bd8a09bc1d77d
maesuke0401 <166385799+maesuke0401@users.noreply.github.com>
2024-06-03 13:41:10 +0900

    Update item_order.rb

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -7,7 +7,7 @@ class ItemOrder
     validates :item_id
 
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :source_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :source_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
     validates :city
     validates :phone, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
     validates :house_number
